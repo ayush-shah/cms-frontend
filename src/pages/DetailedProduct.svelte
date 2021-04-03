a738e85468e6a99e4391afb98b98dc74119e7b77
Ayush Shah <ayush.x.shah@nielsen.com>
2021-04-03 19:13:30 +0530

    Add and Delete product via UI implemented

--- src/pages/DetailedProduct.svelte
+++ src/pages/DetailedProduct.svelte
@@ -1,9 +1,28 @@
 <script>
+  let deleteSuccess = false;
   async function getProductDetails() {
     const resproductDetails = await fetch(
-      `http://${location.hostname}:3000/product/${id}`
+      `http://${location.hostname}:3000/product/id=${id}`
     );
     return await resproductDetails.json();
+  }
+  async function deletePrompt(id) {
+    if (confirm("Do you want me to delete?")) {
+      const deleteConfirmation = await fetch(
+        `http://${location.hostname}:3000/product/delete/${id}`,
+        { method: "delete" }
+      );
+      if (deleteConfirmation.status === 200) {
+        deleteConfirmation.json().then(async () => {
+          deleteSuccess = true;
+          let timeout = await setTimeout(() => {
+            deleteSuccess = false;
+          }, 2000);
+        });
+      } else {
+        alert("Something went wrong");
+      }
+    }
   }
   export let id;
 </script>
@@ -12,6 +31,11 @@
   Loading...
 {:then product}
   <div class="product-wrapper" id={product["_id"]}>
+    {#if deleteSuccess}
+      <div class="delete-success" bind:this={deleteSuccess}>
+        Deleted Successfully
+      </div>
+    {/if}
     <img
       src={product.imageLink}
       alt={product.productName}
@@ -43,6 +67,14 @@
         </h5>
       </div>
       <div class="addToCart">Add to cart</div>
+      <div
+        class="removeProduct"
+        on:click={() => {
+          deletePrompt(product["_id"]);
+        }}
+      >
+        <i class="gg-trash" />
+      </div>
     </div>
   </div>
 {:catch error}
@@ -55,13 +87,17 @@
     margin: 10%;
     aspect-ratio: 3/4;
     object-fit: cover;
+    flex: 0 1;
   }
 
   .product-wrapper {
     display: flex;
     flex-flow: row wrap;
+    position: relative;
+    padding: 0 5vw;
   }
   .detail-flex-wrapper {
+    flex: 1 0;
     display: flex;
     flex-flow: column wrap;
     align-self: center;
@@ -77,6 +113,72 @@
   }
   .addToCart {
     margin: 5% 0;
+    width: 50%;
+  }
+  .delete-success {
+    opacity: 0;
+    background: #7ace7a;
+    padding: 15px 5vw;
+    position: absolute;
+    color: #fff;
     width: 100%;
+    left: 0;
+    top: 0;
+    animation: show 2s linear both;
+  }
+  @keyframes show {
+    from {
+      opacity: 0;
+    }
+    15% {
+      opacity: 1;
+    }
+    85% {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
+  }
+  .gg-trash {
+    box-sizing: border-box;
+    position: relative;
+    display: block;
+    transform: scale(var(--ggs, 1));
+    width: 10px;
+    height: 12px;
+    border: 2px solid transparent;
+    box-shadow: 0 0 0 2px, inset -2px 0 0, inset 2px 0 0;
+    border-bottom-left-radius: 1px;
+    border-bottom-right-radius: 1px;
+    margin-top: 4px;
+  }
+
+  .gg-trash::after,
+  .gg-trash::before {
+    content: "";
+    display: block;
+    box-sizing: border-box;
+    position: absolute;
+  }
+
+  .gg-trash::after {
+    background: currentColor;
+    border-radius: 3px;
+    width: 16px;
+    height: 2px;
+    top: -4px;
+    left: -5px;
+  }
+
+  .gg-trash::before {
+    width: 10px;
+    height: 4px;
+    border: 2px solid;
+    border-bottom: transparent;
+    border-top-left-radius: 2px;
+    border-top-right-radius: 2px;
+    top: -7px;
+    left: -2px;
   }
 </style>
--- src/pages/Products.svelte
+++ src/pages/Products.svelte
@@ -1,9 +1,123 @@
 <script>
-    
+  async function categories() {
+    const categories = await fetch("http://localhost:3000/product/category");
+    return categories.json().then((result) => {
+      return [...new Set(result)];
+    });
+  }
+  let productName, category, price, quantity, description, imageLink;
+  async function submitFormData() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      productName: productName.value,
+      category: category.value,
+      price: price.value,
+      quantity: quantity.value,
+      description: description.value,
+      imageLink: imageLink.value,
+    });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:3000/product/insert", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }
 </script>
 
-Products Here
+<form
+  on:submit|preventDefault={submitFormData}
+  class="insert-product"
+  autocomplete="off"
+>
+  <label for="productName">Product Name</label>
+  <input
+    type="text"
+    name="productName"
+    placeholder="Product Name"
+    required
+    bind:this={productName}
+  />
+  <label for="category">Category</label>
+  <input
+    list="category"
+    placeholder="Select Category"
+    name="category"
+    bind:this={category}
+  />
+  <datalist id="category">
+    {#await categories()}
+      Loading...
+    {:then category}
+      {#each category as category}
+        <option value={category}>{category}</option>
+      {/each}
+    {/await}
+  </datalist>
+  <label for="price">Price</label>
+  <input
+    type="number"
+    name="price"
+    step="0.01"
+    placeholder="Price"
+    required
+    bind:this={price}
+  />
+  <label for="quatity">Quantity</label>
+  <input
+    type="number"
+    name="quantity"
+    placeholder="Quantity"
+    required
+    bind:this={quantity}
+  />
+  <label for="description">Description</label>
+  <textarea
+    placeholder="Description Here.."
+    name="description"
+    required
+    bind:this={description}
+  />
+  <label for="imageLink">Upload Images</label>
+  <input
+    type="text"
+    placeholder="Enter URL"
+    name="imageLink"
+    bind:this={imageLink}
+  />
+  <input type="submit" value="Submit" />
+</form>
 
-<style>
+<style scoped>
+  form.insert-product {
+    width: 50%;
+    min-height: 50%;
+    margin: auto;
+    position: relative;
+    display: flex;
+    padding: 15px;
+    flex-flow: column;
+  }
 
+  form.insert-product > * {
+    flex: 1 0;
+    border: none;
+    /* box-shadow: 1px 1px 4px #5555; */
+    margin: 10px 0;
+  }
+
+  form.insert-product input,
+  form.insert-product textarea {
+    box-shadow: 1px 4px 4px #0005;
+    padding: 8px 15px;
+    outline: none;
+  }
 </style>
